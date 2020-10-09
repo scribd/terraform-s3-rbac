687c7886a14983f93c7e14ce57cf9b981017e7a7
Jason Davila <jasond@scribd.com>
2020-10-09 09:14:27 -0700

    Allow public options
    Input vars for public ACLs and public access. Options default to not
    public.

--- vars.tf
+++ vars.tf
@@ -24,3 +24,33 @@ variable "role_name" {
   type        = string
   description = "name to give your role that will be able to be assume by remote principal(s)"
 }
+
+variable "acl" {
+  type        = string
+  default     = "private"
+  description = "The canned ACL to apply."
+}
+
+variable "block_public_acls" {
+  type        = string
+  default     = true
+  description = "PUT Object calls will fail if the request includes an object ACL."
+}
+
+variable "block_public_policy" {
+  type        = string
+  default     = true
+  description = "Reject calls to PUT Bucket policy if the specified bucket policy allows public access."
+}
+
+variable "restrict_public_buckets" {
+  type        = string
+  default     = true
+  description = "Ignore public ACLs on this bucket and any objects that it contains."
+}
+
+variable "ignore_public_acls" {
+  type        = string
+  default     = true
+  description = "Only the bucket owner and AWS Services can access this buckets if it has a public policy."
+}
